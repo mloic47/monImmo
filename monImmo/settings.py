f8d610dc2bdc8ea6f7f78806fbf8e33690c613e6
leroyloic <loic.mewoulou@cm.maviance.com>
2024-04-11 11:22:35 +0100

    removed sqlite databse file

--- monImmo/settings.py
+++ monImmo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'monImmo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monImmo',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',  # Or the hostname of your MySQL server
+        'PORT': '3306',       # MySQL default port is 3306
     }
 }
 
