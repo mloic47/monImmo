46873e97acb7ee73ab2281b11cd3d95ce32fee8e
leroyloic <loic.mewoulou@cm.maviance.com>
2024-04-30 12:47:28 +0100

    render all html pages

--- appMonImmo/views.py
+++ appMonImmo/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html')
 
-def index(request):
-    return HttpResponse("Bienvenue sur monImmo. Un reseau social pour immobilier.")
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def view_property(request):
+    return render(request, 'view_property.html')   
+
+def search(request):
+    return render(request, 'search.html')
+
+def about(request):
+    return render(request, 'about.html')
--- monImmo/urls.py
+++ monImmo/urls.py
@@ -1,35 +1,13 @@
-"""
-URL configuration for monImmo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from appMonImmo import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('login/', include('monImmo.urls')),
-    # path('register/', include('monImmo.urls')),
-    # path('logout/', include('monImmo.urls')),
-    # path('home/', include('monImmo.urls')),
-    # path('property/add/', include('monImmo.urls')),
-    # path('property/edit/<int:id>/', include('monImmo.urls')),
-    # path('property/delete/<int:id>/', include('monImmo.urls')),
-    # path('property/<int:id>/', include('monImmo.urls')),
-    # path('property/', include('monImmo.urls')),
-    # path('search/', include('monImmo.urls')),
-    # path('contact/', monImmo.views.contact, name='contact'),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+    path('contact/', views.contact, name='contact'),
+    path('view_property/', views.view_property, name='view_property'),
+    path('search/', views.search, name='search'),
+    path('about/', views.about, name='about'),
 ]
